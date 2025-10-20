ecc046c07a9e1e546db282756f92d202c4966ae9
Mohd Intikhab <mohdintikhab432@gmail.com>
2025-10-21 03:22:30 +0530

    Go to Initial

--- app.js
+++ app.js
@@ -99,11 +99,11 @@ app.use((req,res, next)=>{
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter); 
+
 app.get("/", (req, res) => {
   res.redirect("/listings");  // or res.render("home") if you have a homepage template
-}); 
+});  
 app.use("/", userRouter);
-
 app.use((err, req, res, next)=>{
   let{statusCode=500, message="Something went wrong"} = err; 
   // res.status(statusCode).send(message); 
