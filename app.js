0c99e907e630e9cf8eb73ee716bd1f7d0ad77ac0
Mohd Intikhab <mohdintikhab432@gmail.com>
2025-10-21 02:35:49 +0530

    fix: move root redirect before user routes

--- app.js
+++ app.js
@@ -98,11 +98,11 @@ app.use((req,res, next)=>{
 // });
 
 app.use("/listings", listingRouter);
-app.use("/listings/:id/reviews", reviewRouter);
-app.use("/", userRouter); 
+app.use("/listings/:id/reviews", reviewRouter); 
 app.get("/", (req, res) => {
   res.redirect("/listings");  // or res.render("home") if you have a homepage template
-});
+}); 
+app.use("/", userRouter);
 
 app.use((err, req, res, next)=>{
   let{statusCode=500, message="Something went wrong"} = err; 
